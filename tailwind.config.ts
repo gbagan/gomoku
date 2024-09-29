2179baa59f06bcc3956fa1e60f7a2b39f30b6971
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-09-29 11:11:18 +0200

    tailwind.config.js > tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      height: {
+        '30': '7.5rem',
+      },
+      backgroundImage: {
+        seamless: "url('../seamless.webp')",
+        main: "url('../background.webp')",
+        thinking: "url('../girl_thinking.webp')",
+        happy: "url('../girl_happy.webp')",
+        crying: "url('../girl_crying.webp')",
+        speaking: "url('../girl_speaking.webp')",
+        surprised: "url('../girl_surprised.webp')",
+        "black-peg": "radial-gradient(closest-corner at 30% 25%, rgb(119, 119, 119), rgb(34, 34, 34))",
+        "white-peg": "radial-gradient(closest-corner at 30% 25%, rgb(238, 238, 238), rgb(187, 187, 187))",
+      },
+      gridTemplateColumns: {
+        '20/80': '20% 80%',
+      },
+      boxShadow: {
+        'threat': '0 0 20px red',
+      }
+    }
+  },
+  plugins: [],
+} satisfies Config
